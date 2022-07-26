af003cd43d5928227154fbf136335eb6162561f6
Rafael Carrasco <vantyc@hotmail.com>
2022-07-25 23:59:14 -0500

    Adding applicaton Java source

---
+++ src/main/java/clinic/programming/training/Application2.java
@@ -0,0 +1,14 @@
+package clinic.programming.training;
+
+public class Application2 {
+				    
+				public Application2() {
+								        System.out.println ("Inside Applicationi2");
+				}
+
+				// method main(): ALWAYS the APPLICATION entry point
+				public static void main (String[] args) {
+				    	System.out.println ("Starting Application2");
+				      Application2 app = new Application2();
+			  }
+}
